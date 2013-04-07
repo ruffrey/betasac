6be39d957dd3a148c793c8fd76048ff5b4e95f98
Jeff Parrish <jeffhparrish@gmail.com>
2013-04-07 07:24:00 -0700

    cfg updates

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
 var settings = {
-	secret: "9jk9slsadxj8xHnJkdu7tr6svdhjs7g7",
+	secret: "aaaxxx",
 	
 	Mongo: {
-		db: "beta",
-		user: "betaapp123",
-		pass: "k032j0s9djpsfjjjx99987hOIds8hsipso98",
-		path: "ds035027.mongolab.com:35027"
+		db: "",
+		user: "",
+		pass: "",
+		path: ""  // include port without trailing forward slash
 	},
 	
 	Redis: {
-		host: '50.30.35.9', 
-		port: 2491, 
-		db: "beta",
-		pass: "d5b6c54c9e60927ea68c9e1f5fccd766"
+		host: '',  // ip
+		port: , 
+		db: "",
+		pass: ""
 	},
 	
 	auth: {
-		BASE: "http://localhost:3000/auth/",
-		REALM: 'http://localhost:3000/'
+		BASE: "http://localhost:3000/auth/", // include trailing forward slash
+		REALM: 'http://localhost:3000/' // include trailing forward slash
 	}
 };
 
